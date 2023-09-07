9b415fed96f385ef31e51db71686fa163b5dc4b4
Florent BOURASSE <fbourasse@jahia.com>
2023-09-07 08:38:34 +0200

    Correction of queries to delete JBPM data
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/main/java/org/jahia/community/workflowtaskscleaner/CleanCommand.java
+++ src/main/java/org/jahia/community/workflowtaskscleaner/CleanCommand.java
@@ -20,12 +20,12 @@ public class CleanCommand implements Action {
     private static final Logger LOGGER = LoggerFactory.getLogger(CleanCommand.class);
     private static final String HUMAN_READABLE_FORMAT = "d' days 'H' hours 'm' minutes 's' seconds'";
     private static final List<String> SQL_STMTS = Arrays.asList(
-            "DELETE FROM jbpm_people_assignm_pot_owners WHERE task_id IN (SELECT task_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT id FROM jbpm_process_instance_info))",
-            "DELETE FROM jbpm_people_assignments_bas WHERE task_id IN (SELECT task_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT id FROM jbpm_process_instance_info))",
-            "DELETE FROM jbpm_i18ntext WHERE task_subjects_id IN (SELECT task_subjects_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT id FROM jbpm_process_instance_info))",
-            "DELETE FROM jbpm_i18ntext WHERE task_names_id IN (SELECT task_names_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT id FROM jbpm_process_instance_info))",
-            "DELETE FROM jbpm_i18ntext WHERE task_descriptions_id IN (SELECT task_descriptions_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT id FROM jbpm_process_instance_info))",
-            "DELETE FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT id FROM jbpm_process_instance_info)"
+            "DELETE FROM jbpm_people_assignm_pot_owners WHERE task_id IN (SELECT task_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT instance_id FROM jbpm_process_instance_info))",
+            "DELETE FROM jbpm_people_assignments_bas WHERE task_id IN (SELECT task_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT instance_id FROM jbpm_process_instance_info))",
+            "DELETE FROM jbpm_i18ntext WHERE task_subjects_id IN (SELECT task_subjects_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT instance_id FROM jbpm_process_instance_info))",
+            "DELETE FROM jbpm_i18ntext WHERE task_names_id IN (SELECT task_names_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT instance_id FROM jbpm_process_instance_info))",
+            "DELETE FROM jbpm_i18ntext WHERE task_descriptions_id IN (SELECT task_descriptions_id FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT instance_id FROM jbpm_process_instance_info))",
+            "DELETE FROM jbpm_task WHERE (status='Completed' OR status='Exited') AND PROCESS_INSTANCE_ID NOT IN (SELECT instance_id FROM jbpm_process_instance_info)"
     );
     
     @Override
